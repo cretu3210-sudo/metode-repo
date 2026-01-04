ee16b061cee52c914a789c32fec85fe646b13cc8
Cristian Cretu <cretu.3210@gmail.com>
2026-01-04 19:37:06 +0300

    Add exemplu2

--- src/Exemplu.java
+++ src/Exemplu.java
@@ -1,5 +1,12 @@
 public class Exemplu {
     public static void main(String[] args) {
+        displayAddress();
+        displayAddress();
+    }
+
+    static void displayAddress() {
+
+
         System.out.println("Compania ABC");
         System.out.println("str. Columna 140, MD-2004");
         System.out.println("Chisinau, Republica Moldova");
